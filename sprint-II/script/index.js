fa76359ac395a631cd2f9807ac7f91755179407c
jhjshin <hyunjoo940926@gmail.com>
2020-04-30 21:09:47 -0400

    modified eventHandler/eventListener in shorter form

--- sprint-II/script/index.js
+++ sprint-II/script/index.js
@@ -5,11 +5,12 @@ let nameInput = document.querySelector('.join-input__name');
 let commentInput = document.querySelector('.join-input__comment');
 let formSubmit = document.querySelector('.join-input--right');
 
-let joinContainer = document.querySelector('.join-container--inner');
+let innerContainer = document.querySelector('.join-container--inner');
+// let joinContainer = document.querySelector('.join-container');
 
 
 // TimeStamp
-function generateTime() {
+function updateTime() {
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth();
@@ -20,15 +21,36 @@ function generateTime() {
 };
 
 
-function btnHandler (event) {
-    event.preventDefault();
-    if (nameInput.value === undefined || nameInput.value === ''|| nameInput.value === null) {
-        alert ('Please type your name!');        
-    } else {
-        // Create Box
+// Array for default comments
+commentSection = [
+    {
+        'name': 'Micheal Lyons',
+        'date': '12/18/2018',
+        'comment': 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
+    },
+    {
+        'name': 'Gary Wong',
+        'date': '12/12/2018',
+        'comment': 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He\'s so talented! I wish I can ride like him one dayt so I can really enjoy myself!'
+    },
+    {
+        'name': 'Theodore Duncan',
+        'date': '11/15/2018',
+        'comment': 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He\'s definitely my favorite ever!'
+    }
+];
+
+// const commentReversed = commentSection.reverse();
+
+// btnHandler function
+function btnHandler () {
+    
+    for (var i = 0; i < commentSection.length; i++) {
+
+        // Create Elements
         let commentBox = document.createElement('div'); 
         commentBox.classList.add('join-container__box');
-        
+
         let commentBoxLeft = document.createElement('div');
         commentBoxLeft.classList.add('join-container--left');
         commentBox.appendChild(commentBoxLeft);
@@ -37,74 +59,59 @@ function btnHandler (event) {
         commentBoxRight.classList.add('join-container--right');
         commentBox.appendChild(commentBoxRight);
 
-
         // Name
         let newName = document.createElement('p');
         newName.classList.add('join-container__name');
-
         commentBoxRight.appendChild(newName);
-        newName.innerText = nameInput.value;
-
-
+        
         // Time
-        let showTime = document.createElement('p');
-        showTime.classList.add('join-container__time');
-        commentBoxRight.appendChild(showTime);
-
-        showTime.innerText = generateTime();
-
+        let newTime = document.createElement('p');
+        newTime.classList.add('join-container__time');            
+        commentBoxRight.appendChild(newTime);          
 
         // Comment
         let newComment = document.createElement('div');
         newComment.classList.add('join-container__comment');
-
         commentBoxRight.appendChild(newComment);
-        newComment.innerText = commentInput.value;
 
-        joinContainer.appendChild(commentBox);
+        innerContainer.appendChild(commentBox);
+
+        newName.innerText = commentSection[i].name;
+        newTime.innerText = commentSection[i].date;
+        newComment.innerText = commentSection[i].comment;    
     }
-}
-
-// eventlistener
-formSubmit.addEventListener('submit', btnHandler);
-
-
-// DEFAULT COMMENTS
-
-joinContainer.onload = function() {
-
-    defaultComments = [
-        comm1 = {
-            name: 'Micheal Lyons',
-            date: '12/18/2018',
-            comment: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
-        },
-        comm2 = {
-            name: 'Gary Wong',
-            date: '12/12/2018',
-            comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He\'s so talented! I wish I can ride like him one dayt so I can really enjoy myself!'
-        },
-        comm3 = {
-            name: 'Theodore Duncan',
-            date: '11/15/2018',
-            comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He\'s definitely my favorite ever!'
-        }
-    ];
-
-}
-
-
-
-//버튼만들기+입력값 전달 
-
-// function pressBtn(){ 
-//     const currentVal = inputBar.value; 
     
-//     if(!currentVal.length){ 
-//         alert("댓글을 입력해주세요!!"); 
-//     }else{ 
-//         showComment(currentVal); 
-//         mainCommentCount.innerHTML++; 
-//         inputBar.value =''; 
-//     } 
-// }
+    // empty the array
+    function empty() {
+        commentSection.length = 0;
+    }
+    empty();        
+}
+
+
+btnHandler();
+
+
+
+
+// btnHandler eventlistener
+formSubmit.addEventListener('submit', event => {
+    event.preventDefault();
+
+    // push array
+    commentSection.unshift({
+        'name' : nameInput.value,
+        'date' : updateTime(),
+        'comment' : commentInput.value
+    });
+
+
+    if (nameInput.value === undefined || nameInput.value === ''|| nameInput.value === null) {
+        alert ('Please type your name!');        
+    } else {
+        
+        btnHandler ();
+    
+    }
+
+});
