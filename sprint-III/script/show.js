d5cef3018cc7827cbf8e49b82a0425b2105f38bf
jhjshin <hyunjoo940926@gmail.com>
2020-05-09 23:44:03 -0400

    shortened all DOM in shows page using function

--- sprint-III/script/show.js
+++ sprint-III/script/show.js
@@ -26,11 +26,15 @@ getShowsfromAPI = () => {
 }
 getShowsfromAPI();
 
-function createElem (para1, para2, para3, para4, para5) {
-    return `let ${para1} = document.createElement(${para2}).classList.add(${para3});
-            ${para4}.appendChild(${para1});
-            ${para1}.innerText = ${para5}`
-}
+
+// function for creating element, class name, appendChild, innerText
+function createElem(elem, className, parent, text){
+    let newElement = document.createElement(elem);
+    newElement.classList.add(className);
+    parent.appendChild(newElement);
+    newElement.innerText = text
+    return newElement;
+  }
 
 
 //function for creating new rows
@@ -39,57 +43,27 @@ function getShows (showInfo) {
     // //Create Elements
     let showTable = document.createElement('div'); //container <- table
     let showBody = document.createElement('div'); // table <- body
-    // let showDateHeader = document.createElement('p');
-    // let showDate = document.createElement('p');
-    // let showVenueHeader = document.createElement('p');
-    // let showVenue = document.createElement('p');
-    // let showLocationHeader = document.createElement('p');
-    // let showLocation = document.createElement('p');
-    // let showTicketBtn = document.createElement('button');
-    
-    
+    let showTicketBtn = document.createElement('button');
+    showTicketBtn.innerText = 'BUY TICKETS';
 
     // //Adding class names
     showTable.classList.add('show__table');
     showBody.classList.add('show__body');
-    // showDateHeader.classList.add('show__header');
-    // showDate.classList.add('show__date');
-    // showVenueHeader.classList.add('show__header');
-    // showVenue.classList.add('show__venue');
-    // showLocationHeader.classList.add('show__header');
-    // showLocation.classList.add('show__location');
-    // showTicketBtn.classList.add('show__btn', 'btn');
-
+    showTicketBtn.classList.add('show__btn', 'btn');
 
     // //appendChild in parent
     showContainer.appendChild(showTable);
     showTable.appendChild(showBody);
-    // showBody.appendChild(showDateHeader);
-    // showBody.appendChild(showDate);
-    // showBody.appendChild(showVenueHeader);
-    // showBody.appendChild(showVenue);
-    // showBody.appendChild(showLocationHeader);
-    // showBody.appendChild(showLocation);
-    // showBody.appendChild(showTicketBtn);
 
-    // createElem('showTable', 'div', 'show__table', 'showContainer');
-    // createElem('showBody', 'div', 'show__body', 'showTable');
-    createElem(showDateHeader, 'p', 'show__header', showBody, 'DATE');
-    createElem(showDate, 'p', 'show__date', showBody, showInfo.date);
-    createElem(showVenueHeader, 'p', 'show__header', showBody, 'VENUE');
-    createElem(showVenue, 'p', 'show__venue', showBody, showInfo.place);
-    createElem(showLocationHeader, 'p', 'show__header', showBody, 'LOCATION');
-    createElem(showLocation, 'p', 'show__location', showBody, showInfo.location);
-    createElem(showTicketBtn, 'button', 'show__btn', showBody, 'BUY TICKETS');
+    createElem('p', 'show__header', showBody, 'DATE');
+    createElem('p', 'show__date', showBody, showInfo.date);
+    createElem('p', 'show__header', showBody, 'VENUE');
+    createElem('p', 'show__venue', showBody, showInfo.place);
+    createElem('p', 'show__header', showBody, 'LOCATION');
+    createElem('p', 'show__venue', showBody, showInfo.place);
 
-    // showDateHeader.innerText = 'DATE';
-    // showDate.innerText = showInfo.date;
-    // showVenueHeader.innerText = 'VENUE';
-    // showVenue.innerText = showInfo.place;
-    // showLocationHeader.innerText = 'LOCATION';
-    // showLocation.innerText = showInfo.location;
-    // showTicketBtn.innerText = 'BUY TICKETS';
 
+    showBody.appendChild(showTicketBtn);
 }
 
 
@@ -98,28 +72,14 @@ function getShows (showInfo) {
  they can take same portion of width as the table body */
 
 function tabletHeader () {
+
     let topHeader = document.createElement('div');
-    let topDate = document.createElement('p');
-    let topVenue = document.createElement('p');
-    let topLocation = document.createElement('p');
-    let topBtn = document.createElement('button');
-
     topHeader.classList.add('show__top');
-    topDate.classList.add('show__top-date');
-    topVenue.classList.add('show__top-venue');
-    topLocation.classList.add('show__top-location');
-    topBtn.classList.add('show__top-btn', 'btn');
 
-    topHeader.appendChild(topDate);
-    topHeader.appendChild(topVenue);
-    topHeader.appendChild(topLocation);
-    topHeader.appendChild(topBtn);
+    createElem('p', 'show__top-date', topHeader, 'DATE');
+    createElem('p', 'show__top-venue', topHeader, 'VENUE');
+    createElem('p', 'show__top-location', topHeader, 'LOCATION');
+    createElem('button', 'show__top-btn', topHeader, 'BUY TICKETS');
 
     showContainer.prepend(topHeader);
-
-    topDate.innerText = 'DATE';
-    topVenue.innerText = 'VENUE';
-    topLocation.innerText = 'LOCATION';
-    topBtn.innerText = 'BUY TICKETS';
-
 }
